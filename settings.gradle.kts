78757786e8ba9a497abd03e5b281442fadb4060a
Subhanshu Bansal <subhashu20135@iiitd.ac.in>
2023-04-09 13:27:08 +0530

    Starter Project

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "REST_API_CRUD-Kotlin_Spring"
---
+++ src/main/kotlin/com/example/rest_api_crudkotlin_spring/RestApiCrudKotlinSpringApplication.kt
@@ -0,0 +1,11 @@
+package com.example.rest_api_crudkotlin_spring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RestApiCrudKotlinSpringApplication
+
+fun main(args: Array<String>) {
+    runApplication<RestApiCrudKotlinSpringApplication>(*args)
+}
