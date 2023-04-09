ba7fe92daa160f820cb14e1cfac90d4c0b9f7d2d
Subhanshu Bansal <subhashu20135@iiitd.ac.in>
2023-04-09 22:39:42 +0530

    Added CRUD Operations

--- src/main/kotlin/com/example/rest_api_crudkotlin_spring/users/UserController.kt
+++ src/main/kotlin/com/example/rest_api_crudkotlin_spring/users/UserController.kt
@@ -1,2 +1,48 @@
 package com.example.rest_api_crudkotlin_spring.users
 
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/api/users")
+class UserController(@Autowired private val userRepository: UserRepository) {
+
+    @GetMapping("")
+    fun getAllUsers(): List<User> = userRepository.findAll().toList()
+
+    @PostMapping("")
+    fun createUser(@RequestBody user: User): ResponseEntity<User> {
+        val savedUser =userRepository.save(user)
+        return ResponseEntity(savedUser, HttpStatus.CREATED)
+    }
+
+    @GetMapping("/{id}")
+    fun getUserById(@PathVariable("id") userId: Int): ResponseEntity<User> {
+        val user = userRepository.findById(userId).orElse(null)
+        return if (user != null) {
+            ResponseEntity(user, HttpStatus.OK)
+        }
+        else {
+            ResponseEntity(HttpStatus.NOT_FOUND)
+        }
+    }
+
+    @PutMapping("/{id}")
+    fun updateUser(@PathVariable("id") userId: Int, @RequestBody user: User): ResponseEntity<User> {
+        val existingUser = userRepository.findById(userId).orElse(null) ?: return ResponseEntity(HttpStatus.NOT_FOUND)
+
+        val updatedUser = existingUser.copy(name = user.name, email = user.email)
+        userRepository.save(updatedUser)
+        return ResponseEntity(updatedUser, HttpStatus.OK)
+    }
+
+    @DeleteMapping("{id}")
+    fun deleteUserById(@PathVariable("id") userId: Int): ResponseEntity<User> {
+        val existingUser = userRepository.findById(userId).orElse(null) ?: return ResponseEntity(HttpStatus.NOT_FOUND)
+
+        userRepository.deleteById(userId)
+        return ResponseEntity(HttpStatus.NO_CONTENT)
+    }
+}
